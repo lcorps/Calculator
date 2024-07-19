ea9ad29ad2c821a781f545d876f12b2067244142
lcorps <lcorps921@gmail.com>
2024-07-19 16:56:01 +0000

    Calculator

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const input = document.querySelector(".input")
+const result = document.querySelector(".result")
+const deleteBtn = document.querySelector("delete")
+const keys = document.querySelectorAll("bottom span")
+
+let answer
+let operation = ""
+let decimalAdded = false
+
+const operators = ["+","-","x","/"]
+
+function handleKeyPress(e){}
