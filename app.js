91be61ede2527a41fd851e4282c500766c25dcb2
lcorps <lcorps921@gmail.com>
2024-07-22 16:48:45 +0000

    Calculator

--- app.js
+++ app.js
@@ -12,3 +12,31 @@ let decimalAdded = false
 const operators = ["+","-","x","/"]
 
 function handleKeyPress(e){}
+ const key = e.target.dataset.key
+ const lastChar = operation[operation.length - 1]
+
+if (key =="="){
+    return
+}
+
+if (key===  '=' && decimalAdded){
+    return
+}
+
+if(operators.indexOf(key)  !==  -1){
+    decimalAdded = false 
+}
+
+if (operation.length === 0 && key === '-') {
+    operation += key
+    input.innerHTML = operation
+    return
+}
+
+if (operation.length === 0 && operators.indexOf(key) !== -1) {
+    return
+}
+
+if (operators.indexOf(lastChar) !== -1 && operators.indexOf(key) !== -1) {
+    operation = operation.replace(/.$)
+}
